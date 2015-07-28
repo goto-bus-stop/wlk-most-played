f56b8defc936bc1348ee6297e95ac28ffb942787
René Kooi <rene@kooi.me>
2015-07-28 23:19:57 +0200

    0.5.0

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SekshiBot Most Played
  * Description: Lists most played songs as recorded in MongoDB by SekshiBot.
- * Version: 0.4.1
+ * Version: 0.5.0
  * Author: WE ♥ KPOP
  * Author URI: https://welovekpop.club/
  * License: MIT
